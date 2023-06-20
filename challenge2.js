29a317984a90b346369f35c7522d0cfdbc6f6e04
kipkorir73 <123023142+kipkorir73@users.noreply.github.com>
2023-06-20 23:30:47 +0300

    Update challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -1,21 +1,23 @@
+// Prompt the user to enter the speed
 const speedString = prompt('Enter speed');
 const speed = parseInt(speedString);
 
+// Function to check the speed
 function speedcheck(speed) {
-    const speedlimit = 70;
-    const kmperdemerit = 5;
-    const maxdemeritpoints = 12;
+    const speedlimit = 70; // Speed limit in km/h
+    const kmperdemerit = 5; // Number of km per demerit point
+    const maxdemeritpoints = 12; // Maximum demerit points allowed
 
     if (speed <= speedlimit) {
-        return "OK";
+        return "OK"; // Speed is within the limit
     } else {
         const demeritpoints = Math.floor((speed - speedlimit) / kmperdemerit);
         if (demeritpoints > maxdemeritpoints) {
-            return "License suspended";
+            return "License suspended"; // License suspended if exceeding maximum demerit points
         } else {
-            return "Points: " + demeritpoints;
+            return "Points: " + demeritpoints; // Return the number of demerit points
         }
     }
 }
 
-console.log(speedcheck(speed));
+console.log(speedcheck(speed)); // Output the result of the speed check
