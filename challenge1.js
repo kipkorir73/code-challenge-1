9a82dd514cebe0e09839d30209847626008d08c1
kipkorir73 <123023142+kipkorir73@users.noreply.github.com>
2023-06-20 23:14:16 +0300

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,22 +1,21 @@
-let score = 65;
+const speedString = prompt('Enter speed');
+const speed = parseInt(speedString);
 
-function calculatorgrade(score) {
-  if (score >= 0 && score <= 100) {
-    if (score >= 80) {
-      return grade = 'A';
-    } else if (score >= 60 && score <= 79) {
-      return grade = 'B';
-    } else if (score >= 50 && score <= 59) {
-      return grade = 'C';
-    } else if (score >= 40 && score <= 49) {
-      return grade = 'D';
+function speedcheck(speed) {
+    const speedlimit = 70;
+    const kmperdemerit = 5;
+    const maxdemeritpoints = 12;
+
+    if (speed <= speedlimit) {
+        return "OK";
     } else {
-      return grade= 'E';
+        const demeritpoints = Math.floor((speed - speedlimit) / kmperdemerit);
+        if (demeritpoints > maxdemeritpoints) {
+            return "License suspended";
+        } else {
+            return "Points: " + demeritpoints;
+        }
     }
-  } else {
-    return 'Invalid score. Score should be between 0 and 100.';
-  }
 }
 
-let grade = calculatorgrade(score);
-console.log("Grade: " + grade);
+console.log(speedcheck(speed));
